f46b37fbaae56f2d04da2018d13d157f8b321d25
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-07-09 18:51:01 -0700

    fixed bug -> export required for create user async function

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyAUuZUgvlmMYBOl_Kg43Eb2sHnEJp4_us4'
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
 
     const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
