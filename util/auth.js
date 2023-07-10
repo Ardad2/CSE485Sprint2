54e816c6271b5c08dcd59d58fbd62fb298b8f3cc
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-07-09 18:37:42 -0700

    created connection to the back-end

---
+++ util/auth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyAUuZUgvlmMYBOl_Kg43Eb2sHnEJp4_us4'
+
+async function createUser(email, password) {
+
+    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+    {
+        email: email, 
+        password: password, 
+        returnSecureToken: true
+    });
+}
